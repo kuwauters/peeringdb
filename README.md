1dcfdbd5aa35069ea4d5e4414313359f18b28009
kurt.wauters@bics.com <kurt.wauters@bics.com>
2019-07-11 09:17:40 +0000

    v2 supporting credentials - output generation - threading

--- README.md
+++ README.md
@@ -5,12 +5,33 @@
 - obj: defines the data that you want to retrieve
     - ixpfx: retrieve the prefix ranges that are used on all IXs
     - lclix `id`: retireve the peers that are only present in the same region as this ix. The `ìd` is the ix id that can be retrieved by searching the ix itself
+    - l: defines the logging that needs to be used for peeringdb. Credentials are mainly needed for retrieving contact details
+    - p: defines the password that needs to be used for peeringdb. Credentials are mainly needd for retrieving contact details
 
 
 ## Output example
 
-id961407@dl0005 peeringdb]$ python peeringdb.py --lclix 2381
+### retrieving all the ip's from the IXs
+`python peeringdb.py --obj ixpfx`
 
-Avanti Communications South Africa;328306;[u'Africa'];[u'WAF-IX, Lagos: Main', u'WAF-IX, Lagos: Main']
-Avanti Communications South Africa;328306;[u'Africa'];[u'WAF-IX, Lagos: Main', u'WAF-IX, Lagos: Main']
-Juniper Solutions;37398;[u'Africa'];[u'WAF-IX, Lagos: Main']
+2001:504:0:2::/64
+206.126.236.0/22
+208.115.136.0/23
+2001:504:0:4::/64
+206.223.118.0/232001:504:0:5::/64
+
+### retrieving local peers on a specific IX
+`python peeringdb.py --lclix 2381 -l JohnDoe -p changmenow`
+
+name;asn;ip;ip6;as_set;max_pfx;max_pfx6;policy;technical;noc;presence_name;region_continent
+Avanti Communications South Africa;328306;196.60.58.12 // 196.60.58.13;2001:43f8:11f0::5:272:1 // 2001:43f8:11f0::5:272:2;AS-328306;0;0;Open;peering@avantiplc.com;;WAF-IX, Lagos: Main // WAF-IX, Lagos: Main;Africa;
+Avanti Communications South Africa;328306;196.60.58.12 // 196.60.58.13;2001:43f8:11f0::5:272:1 // 2001:43f8:11f0::5:272:2;AS-328306;0;0;Open;peering@avantiplc.com;;WAF-IX, Lagos: Main // WAF-IX, Lagos: Main;Africa;
+Juniper Solutions;37398;198.60.58.15;2001:43f8:11f0::9216:1;;0;0;Open;;;WAF-IX, Lagos: Main;Africa;
+
+`python peeringdb.py --lclix 2381`
+
+name;asn;ip;ip6;as_set;max_pfx;max_pfx6;policy;technical;noc;presence_name;region_continent
+Avanti Communications South Africa;328306;196.60.58.12 // 196.60.58.13;2001:43f8:11f0::5:272:1 // 2001:43f8:11f0::5:272:2;AS-328306;0;0;Open;;;WAF-IX, Lagos: Main // WAF-IX, Lagos: Main;Africa;
+Avanti Communications South Africa;328306;196.60.58.12 // 196.60.58.13;2001:43f8:11f0::5:272:1 // 2001:43f8:11f0::5:272:2;AS-328306;0;0;Open;;;WAF-IX, Lagos: Main // WAF-IX, Lagos: Main;Africa;
+Juniper Solutions;37398;198.60.58.15;2001:43f8:11f0::9216:1;;0;0;Open;;;WAF-IX, Lagos: Main;Africa;
+
--- peeringdb.py
+++ peeringdb.py
@@ -5,6 +5,7 @@ import os
 import pprint
 import tqdm
 from concurrent import futures
+from getpass import getpass
 
 class ix():
     def __init__(self):
@@ -36,9 +37,14 @@ class peer():
         self.max_pfx = ''
         self.max_pfx6 = ''
         self.policy = ''
+        self.technical = ''
+        self.noc = ''
+        self.policy = ''
         self.presence = []
         self.presence_name = []
         self.region_continent = []
+        self.ip = []
+        self.ip6 = []
     
     def update_peer (self, input):
         self.name = input['name']
@@ -53,10 +59,20 @@ class peer():
         for netixlan in input['data'][0]['netixlan_set']:
             (self.presence).append(netixlan['ix_id'])
             (self.presence_name).append(netixlan['name'])
+            (self.ip).append(netixlan['ipaddr4'])
+            (self.ip6).append(netixlan['ipaddr6'])
     
     def update_region (self, input):
         if input['data'][0]['region_continent'] not in self.region_continent:
             (self.region_continent).append(input['data'][0]['region_continent'])
+    
+    def update_contact(self, input,contact_attr):
+        '''
+            when credentials are provided the contact details can be retrieved from peeringdb
+        '''
+        for contact in input['data'][0]['poc_set']:
+            if contact['role'].lower() in contact_attr:
+                setattr(self, contact['role'].lower(), contact['email'])
 
 
 
@@ -71,29 +87,32 @@ def check_prompt():
     parser = argparse.ArgumentParser(description = 'interface on top of peeringdb for programmatic abstraction')
     parser.add_argument("--obj", metavar='objects that you want to query on top of peeringdb', help='\"ixpfx\" for all ip\'s of exchange info -- \"peer\" for peer info', required=False)
     parser.add_argument("--lclix", type = int, metavar='the ix on which you are looking for local peers', help=' the ix id from peeringdb', required=False)
+    parser.add_argument("-l", type = str, metavar = 'the login to be used when obtaining confidential info from peeringdb (mail addresses)', required=False)
+    parser.add_argument('-p', type = str, metavar = 'the password to be use when obtaining confidential info from peeringdb (mail addresses)', required=False)
     settings = parser.parse_args()
 
     return settings
 
-def datacollection (api_url, authentication):
+def datacollection (api_url, authentication,cred):
     '''
         execute get for the given url
     '''
     if authentication:
-        return (requests.get(api_url, auth('id961407',getpass()))).json()
+        return (requests.get(api_url, auth=(cred[0],cred[1]))).json()
     else:
         return (requests.get(api_url)).json()
 
 def analyse_peer(peers,attr_lst):
     print('...analysing..')
+    print(';'.join(attr_lst))
     for el in tqdm.tqdm(peers, total=len(peers)):
         if len(el.region_continent) == 1:
             for attr in attr_lst:
-                
                 if isinstance(getattr(el,attr),list):
-                    print(' // '.join(getattr(el, attr)) + ';')                  
+                    print(' // '.join(getattr(el, attr)), end=';')                  
                 else:
-                    print(getattr(el, attr))
+                    print(getattr(el, attr),end = ';')
+            print()
                     
 
 def create_output():
@@ -104,6 +123,7 @@ def create_output():
 
 def main():
     peer_lst = []
+    cred = ('', '')
 
     cfg = yaml.load(open('conf/config.yaml'), Loader=yaml.SafeLoader)
 
@@ -112,58 +132,68 @@ def main():
     
     if search_obj.obj and search_obj.obj == 'ixpfx':
         #perform URL get - retrieve data from peeringdb
-        ntw_rsp = datacollection('{}{}'.format(cfg['base_url'], cfg['ix']),False)
+        ntw_rsp = datacollection('{}{}'.format(cfg['base_url'], cfg['ix']),False,cred)
         
         try:
             #retrieve specific network data
-            for client in ntw_rsp.json()['data']:
-                ntw_client_rsp = datacollection('{}{}lan/{}'.format(cfg['base_url'], cfg['ix'], client['id'] ), False)
+            for client in ntw_rsp['data']:
+                ntw_client_rsp = datacollection('{}{}lan/{}'.format(cfg['base_url'], cfg['ix'], client['id'] ), False,cred)
 
-                for client_ntw in ntw_client_rsp.json()['data']:
+                for client_ntw in ntw_client_rsp['data']:
                     for ixpfx in client_ntw['ixpfx_set']:
                         print(ixpfx['prefix'])
-        except:
-            print('something went wrong in ixpfx')
+        except Exception as e:
+            print('something went wrong in ixpfx \n {}'.format(e))
     elif search_obj.lclix:
         
-        ntw_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['ix'],search_obj.lclix),False)
+        ntw_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['ix'],search_obj.lclix),False,cred)
         
         try:
             print('.looking up IX LANs')
             ix_obj = ix()
             ix_obj.update_ix(ntw_rsp)
             for ixlan in ix_obj.lanset:
-                ixlan_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['ixlan'],ixlan),False)
+                ixlan_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['ixlan'],ixlan),False,cred)
                 
                 for peer_el in ixlan_rsp['data'][0]['net_set']:
                     peer_obj = peer(ix_obj)
                     peer_obj.update_peer(peer_el)
                     peer_lst.append(peer_obj)
                 
-        except Exception, e:
+        except Exception as e:
             print('something went wrong in lclix\n {}'.format(e))
+
+            print('..Looking up peers')
+
+        for peer_el in tqdm.tqdm(peer_lst, total=len(peer_lst)):    
+            if search_obj.l and search_obj.p:
+                cred = (search_obj.l,search_obj.p)
+                presence_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['peer'],peer_el.id),True,cred)
+                peer_el.update_contact(presence_rsp,cfg['contact'])
+                print('AFTER')
+                print(peer_el.technical)
+            else:
+                presence_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['peer'],peer_el.id),False,cred)
+
+            peer_el.update_presence(presence_rsp)
+
+            print('...looking up ix presence / regions')
+            with futures.ThreadPoolExecutor(40) as executor:
+                rsp_lst = []
+                for ix_el in tqdm.tqdm(peer_el.presence, total = len(peer_el.presence)):
+                            
+                    ntw_rsp = executor.submit(datacollection,('{}{}/{}'.format(cfg['base_url'], cfg['ix'],ix_el)),False,cred)
+                    rsp_lst.append(ntw_rsp)
+                    
+                for el in rsp_lst:
+                    peer_el.update_region(el.result())
+
+        analyse_peer(peer_lst, cfg['attr'])
 
     else:
         print('search object not defined, try peeringdb.py -h')
 
-    print('..Looking up peers')
 
-    for peer_el in tqdm.tqdm(peer_lst, total=len(peer_lst)):    
-        presence_rsp = datacollection('{}{}/{}'.format(cfg['base_url'], cfg['peer'],peer_el.id),False)
-        peer_el.update_presence(presence_rsp)
-
-        print('...looking up ix presence / regions')
-        with futures.ThreadPoolExecutor(40) as executor:
-            rsp_lst = []
-            for ix_el in tqdm.tqdm(peer_el.presence, total = len(peer_el.presence)):
-                          
-                ntw_rsp = executor.submit(datacollection,('{}{}/{}'.format(cfg['base_url'], cfg['ix'],ix_el)),False)
-                rsp_lst.append(ntw_rsp)
-                
-            for el in rsp_lst:
-                peer_el.update_region(el.result())
-
-    analyse_peer(peer_lst, cfg['attr'])
     
 
 if __name__ == '__main__':
